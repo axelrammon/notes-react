8b4858ea60ed50db054fc949816021e0c5ced694
Axel Rammon <axelrammon48@gmail.com>
2020-03-11 17:52:40 -0300

    Updates

---
+++ precache-manifest.9311774ff9ca7186a1b04acbdd8cbe3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43d9cfcc35d795ec2a804e3ef426009",
+    "url": "/notes-react/index.html"
+  },
+  {
+    "revision": "f7afe223550eab63ad9c",
+    "url": "/notes-react/static/js/2.b7f4f372.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/notes-react/static/js/2.b7f4f372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd65820dc09ce5ac209c",
+    "url": "/notes-react/static/js/main.5092bc2e.chunk.js"
+  },
+  {
+    "revision": "0206cc1db72a3332fda3",
+    "url": "/notes-react/static/js/runtime-main.0ab188bf.js"
+  }
+]);
